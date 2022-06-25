e87194267fc686e06840c17a2bf846b89a5153a0
Jon Prosser <jon.prosser@capgemini.com>
2022-06-25 10:58:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+// grabbing the elements
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("button clicked")
+
+    formValidation();
+})
+
+let formValidation = () => {
+    if(input.value === ""){
+        msg.innerHTML = "Please add some text to your post"
+        console.log("FAIL")
+    } else {
+        console.log("Success")
+        msg.innerHTML = "";
+        acceptData()
+    }
+}
+
+let data = {}
+
+let acceptData = () => {
+    data["text"] = input.value;
+    console.log(data)
+
+    createPost()
+}
+
+let createPost = () => {
+    posts.innerHTML += `
+    <div>
+      <p>${data.text}</p>
+      <span class="options">
+        <i onClick="editPost(this)" class="fas fa-edit"></i>
+        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+      </span>
+    </div>
+    `;
+    input.value = "";
+  };
+
+  let deletePost = (e) => {
+    e.parentElement.parentElement.remove();
+  }
+
+  let editPost = (e) => {
+    input.value = e.parentElement.previousElementSibling.innerHTML;
+    e.parentElement.parentElement.remove();
+  };
+
